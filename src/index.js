bba088ff7a6e1bcadece45b4d636f6a52ce1d1b3
Ashley Rarieya <ararieya@gmail.com>
2023-10-16 08:23:45 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const taskDescription = document.getElementById(
+      "new-task-description"
+    ).value;
+
+    if (taskDescription.trim() !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = taskDescription;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function () {
+        taskList.removeChild(listItem);
+      });
+
+      listItem.appendChild(deleteButton);
+
+      taskList.appendChild(listItem);
+      document.getElementById("new-task-description").value = "";
+    }
+  });
 });
